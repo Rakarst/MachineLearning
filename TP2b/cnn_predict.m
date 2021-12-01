711aca1bcedd179940eae51ed7c9aec205aa13c9
Rakarst <74706749+Rakarst@users.noreply.github.com>
2021-12-01 17:58:00 +0000

    Last

--- TP2b/cnn_predict.m
+++ TP2b/cnn_predict.m
@@ -3,7 +3,7 @@ function cnn_predict(cnn_net, X_test, y_test)
     pred_Y = predict(cnn_net,X_test,'ExecutionEnvironment','cpu');
     [~,pred_Y] = max(pred_Y');
     pred_Y = categorical(pred_Y);
-    %plotconfusion(y_test,pred_Y');
+    plotconfusion(y_test,pred_Y');
     conf_mat = confusionmat(y_test, pred_Y);
     conf_mat = bsxfun(@rdivide,conf_mat,sum(conf_mat,2))
 end
